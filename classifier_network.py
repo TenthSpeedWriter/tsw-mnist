b5ce2d11f44ed0c8ceb3f2af65fa635788033cbc
TenthSpeedWriter (Adam L. Seale) <addseale2@gmail.com>
2016-02-18 13:47:22 -0600

    Adds multiple docs, assertion of valid keep_prob range in .train

--- classifier_network.py
+++ classifier_network.py
@@ -15,10 +15,15 @@ class ClassifierNetwork:
     
     def train(self, data, rubric,
               learn_rate=tf.constant(1e-2),
-              keep_prob=tf.constant(0.5)):
+              keep_prob=1.0):
         """
         Runs a single training pass on this network given the specified data and rubric.
+        Allows for a specified keep_prob in (0, 1] to reduce
         """
+        if keep_prob <= 0 or keep_prob > 1:
+            err_str = "Keep prob must be >0 and <= 1 (given {0})".format(str(keep_prob))
+            raise ValueError(err_str)
+            
         feed_dict = {
             self.x: data,
             self.rubric: rubric,
@@ -30,10 +35,11 @@ class ClassifierNetwork:
     def accuracy_on_set(self, data, rubric):
         """
         Returns the evaluation of the accuracy of this network given the specified data and rubric.
+        Fixes keep_prob at 100% for full network analysis.
         """
         feed_dict = {
             self.x: data,
             self.rubric: rubric,
-            self.keep_prob: keep_prob
+            self.keep_prob: 1.0
         }
         return self.accuracy.eval(feed_dict=feed_dict)
